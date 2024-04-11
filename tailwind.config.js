55f3ee41889488895943fadf3e6e459abfe41a7f
Desi <141796712+DesiList753@users.noreply.github.com>
2024-04-10 20:52:47 -0400

    pre-deploy

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export const content = ['./src/**/*.{astro,js,jsx,ts,tsx}'];
+export const darkMode = 'media';
+export const theme = {
+  extend: {},
+};
+export const variants = {
+  extend: {},
+};
+export const plugins = [];
